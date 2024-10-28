2386cf7039a4177be29e23e7a2c8ee07c2281780
Md Aminul Islam <mr.aminul118@gmail.com>
2024-10-28 09:24:49 +0600

    Navbar By DaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import DaisyNav from "./components/DaisyNav/DaisyNav";
+
 export default function App() {
   return (
     <>
-      <h1 className="text-5xl">Hello</h1>
+    <DaisyNav></DaisyNav>
+      
     </>
   );
 }
